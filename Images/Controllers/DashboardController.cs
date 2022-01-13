58de358cf77bd47bc1862430c201df90640d7167
khattiyatungklang@hotmail.com <khattiyatungklang@hotmail.com>
2022-01-13 18:44:45 +0700

    Chart API

---
+++ Images/Controllers/DashboardController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Images.Models;
+
+namespace Images.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private Entities1 db = new Entities1();
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            var data = db.Pictures.Select(n => n.Pic_ID).Count();
+            ViewData["Message"] = data;
+            return View();
+        }
+    }
+}
